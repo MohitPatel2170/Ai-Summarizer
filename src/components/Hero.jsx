b2f5e05d83b478b620a4c69e1e0b468941b1644a
Mohit Patel <mohit.patel2170@gmail.com>
2025-04-08 06:07:01 +0530

    Resposive page works good

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
           type="button"
           onClick={() =>
             window.open(
-              "https://github.com/esadakman/reactjs-ai-summarizer",
+              "https://github.com/MohitPatel2170/Ai-Summarizer",
               "_blank"
             )
           }
